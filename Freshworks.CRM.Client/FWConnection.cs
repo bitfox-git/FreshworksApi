3b96d5c4aeb489396e7a0043965a4b434b406e0f
Lucas Vos <vos.lucas@outlook.com>
2021-03-25 14:25:17 +0100

    Initial commit

---
+++ Freshworks.CRM.Client/FWConnection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace Freshworks.CRM.Client
+{
+    class FWConnection
+    {
+
+        private static HttpClient client = new HttpClient();
+
+        private string subdomain;
+        private string apikey;
+
+
+        public FWConnection(string subdomain, string apikey)
+        {
+            this.subdomain = subdomain;
+            this.apikey = apikey;
+        }
+
+
+        public List<T> GetSelectorAsync<T>()
+        {
+
+        }
+        
+
+        private GetApiRequets(string api, string optinos)
+        {
+
+            
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri($"{baseUrl}/token"),
+                Headers = {
+                    { HttpRequestHeader.Authorization.ToString(), $"Basic {base64credentials}" }
+
+                }
+            };
+
+
+            var result = await client.SendAsync(httpRequestMessage);
+            var content = await result.Content.ReadAsStringAsync();
+            var x = JsonConvert.DeserializeObject<TokenResult>(content);
+            return x.token;
+
+
+            var requets = client.GetAsync()
+
+        }
+
+    }
+}
---
+++ Freshworks.CRM.Client/IFWConnection.cs
@@ -0,0 +1,10 @@
+﻿
+using System;
+
+namespace Freshworks.CRM.Client
+{
+    public interface IFWConnection
+    {
+
+    }
+}
