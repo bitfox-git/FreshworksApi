08fecb1c51a97f94c7e84310583dca1cae5812d8
Niek Tuytel <n.tuytel@dutchgrit.nl>
2021-09-29 10:40:06 +0200

    Remove bug

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -13,26 +13,27 @@ namespace Sample
     {
         static async Task Main(string[] args)
         {
+            Console.WriteLine("called");
 
-            //create an instance
-            var conn = new CRMClient("<your prefix>", "<yourkey>");
-
-
-            //get an account
-            var account = await conn.GetByID<SalesAccount>(30002095168);
-
-            var cf = account.GetCustomFields<CustomSalesAccountFields>();
-
-            Console.WriteLine(cf);
-            cf.cf_external_id = "13";
-            account.city = "Zwijndrecht";
-            account.SetCustomFields(cf);
-            var account2 = await conn.Update(account);
+            ////create an instance
+            //var conn = new CRMClient("<your prefix>", "<yourkey>");
 
 
-            var a2test = (JObject)(account2.custom_field);
-            var a2test2 = a2test.ToObject<CustomSalesAccountFields>();
-            Console.WriteLine(a2test2.cf_external_id);
+            ////get an account
+            //var account = await conn.GetByID<SalesAccount>(30002095168);
+
+            //var cf = account.GetCustomFields<CustomSalesAccountFields>();
+
+            //Console.WriteLine(cf);
+            //cf.cf_external_id = "13";
+            //account.city = "Zwijndrecht";
+            //account.SetCustomFields(cf);
+            //var account2 = await conn.Update(account);
+
+
+            //var a2test = (JObject)(account2.custom_field);
+            //var a2test2 = a2test.ToObject<CustomSalesAccountFields>();
+            //Console.WriteLine(a2test2.cf_external_id);
             
 
       
