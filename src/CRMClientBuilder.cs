864f933e00138f555ad72b4e9706721c2664351b
Lucas Vos <vos.lucas@outlook.com>
2021-10-01 13:17:08 +0200

    Quick changes

---
+++ src/CRMClientBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitfox.Freshworks
+{
+    /// <summary>
+    /// Factory the get a ICRMClient instance.\
+    /// It uses a Fluent API pattern.
+    /// </summary>
+    public class CRMClientBuilder
+    {
+        private string subdomain;
+
+        private string apikey;
+
+
+        public CRMClientBuilder SetApiKey(string apikey)
+        {
+            this.apikey = apikey;
+            return this;
+        }
+
+        public CRMClientBuilder SetSubdomain(string subdomain)
+        {
+            this.subdomain = subdomain;
+            return this;
+        }
+
+        public ICRMClient Build()
+        {
+            return new CRMClient(subdomain, apikey);
+
+        }
+
+    }
+}
