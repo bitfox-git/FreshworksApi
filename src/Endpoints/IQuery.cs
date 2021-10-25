20e18bac917bc62d2e3420a3813300f468738e94
Niek Tuytel <n.tuytel@dutchgrit.nl>
2021-10-25 14:18:02 +0200

    Remove ISearch from IQuery

--- src/Endpoints/IQuery.cs
+++ src/Endpoints/IQuery.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Bitfox.Freshworks.Endpoints
 {
-    public interface IQuery: ISelector, ISearch
+    public interface IQuery: ISelector
     {
         /// <summary>
         /// Set Tables that add on response 
@@ -85,6 +85,29 @@ namespace Bitfox.Freshworks.Endpoints
         /// </summary>
         /// <typeparam name="T">Model data used in response</typeparam>
         Task<Result<T>> GetAllFields<T>() where T : IHasFields;
+
+
+        /// <summary>
+        /// Search in content what do actually match
+        /// </summary>
+        /// <param name="query">returning data contains this query</param>
+        Task<Result<Search>> SearchOnQuery(string query);
+
+        /// <summary>
+        /// Get all content of TEntity from database, than filter data on SearchOnFilter model.
+        /// </summary>
+        /// <typeparam name="TEntity">Model you want to get data from</typeparam>
+        /// <param name="body">The Value that you want to filter</param>
+        Task<Result<SearchFilter>> SearchOnFilter<TEntity>(SearchFilter body) where TEntity : IHasFilteredSearch;
+
+        /// <summary>
+        /// Lookup in data with some filters specified.
+        /// </summary>
+        /// <param name="query">lookup for this string in models</param>
+        /// <param name="field">data from given field ()</param>
+        /// <param name="entities">entities to get data from</param>
+        /// <returns></returns>
+        Task<Result<SearchLookup>> SearchOnLookup(string query, string field, string entities);
 
     }
 }
